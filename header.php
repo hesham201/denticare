c44eb8b681890eeee0057f231e905fa658144c0f
hesham112 <107617341+hesham201@users.noreply.github.com>
2023-01-18 17:03:23 +0500

    changes

---
+++ header.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="assets/bootstrap-5.2.3/dist/css/bootstrap.min.css" />
+    <script src="assets/bootstrap-5.2.3/dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="assets/fontawesome-free-6.2.1-web/css/all.min.css">
+    <script src="assets/fontawesome-free-6.2.1-web/js/all.min.js"></script>
+    <link rel="stylesheet" href="assets/OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="assets/OwlCarousel2-2.3.4/dist/owl.carousel.min.js"></script>
+    <script src="assets/js/script.js"></script>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Document</title>
+</head>
---
+++ navbar.php
@@ -0,0 +1,22 @@
+<section class="top-nav">
+    <div class="wrap">
+        <div class="d-flex justify-content-end">
+            <div>
+                <span><i class="fa fa-clock"></i></span>
+                <h6>Monday to Friday</h6>
+                <p>8AM - 9PM</p>
+            </div>
+            <div>
+                <span><i class="fa fa-location-dot"></i></span>
+                <h6>Address</h6>
+                <p>Deák Ferenc Street 19, Budapest, Hungary</p>
+
+            </div>
+            <div>
+                <button>
+                    <span><i class="fa-solid fa-phone"></i></span>+36 55 540 069
+                </button>
+            </div>
+        </div>
+    </div>
+</section>
