88064f664c353aa04c785fa040829b77d0362391
hesham112 <107617341+hesham201@users.noreply.github.com>
2023-01-20 18:19:35 +0500

    changes

--- index.php
+++ index.php
@@ -47,8 +47,11 @@ include_once "header.php"
                 <div class="desc">
                     <span class="tag">Creating</span>
                     <h2>Brighter Smiles</h2>
-                    <p class="carsl_cnt">A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-                    <p class="carsl_btn"><a href="#" class="btn-gradient-bg">Learn More</a></p>
+                    <div class="carsl_cnt d-flex">
+                        <button class="green-type">Check Out Our Services</button>
+                        <button class="inherit-bg">View Dental Portfolio</button>
+                    </div>
+                    <!-- <p class="carsl_btn"><a href="#" class="btn-gradient-bg">Learn More</a></p> -->
                 </div>
             </div>
             <div class="item">
@@ -64,22 +67,22 @@ include_once "header.php"
                     </div>
                     <!-- <p class="carsl_cnt">A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p> -->
                     <div class="experience-holdays">
-                        <div>
+                        <div class="exp-heading bg-green-type">
                             <h2>Experience Dental Holidays</h2>
                         </div>
-                        <div>
+                        <div class="bg-blue-type">
                             <span><i class="fa-solid fa-jet-fighter-up"></i></span>
                         </div>
-                        <div>
+                        <div class="bg-blue-type">
                             <span><i class="fa-solid fa-globe"></i></span>
                         </div>
-                        <div>
+                        <div class="bg-blue-type">
                             <span><i class="fa-solid fa-hospital"></i></span>
                         </div>
-                        <div>
+                        <div class="bg-blue-type">
                             <span><i class="fa-solid fa-tooth"></i></span>
                         </div>
-                        <div>
+                        <div class="bg-blue-type">
                             <button>Start Your Dental Travel</button>
                         </div>
                     </div>
@@ -89,7 +92,236 @@ include_once "header.php"
         </div>
     </section>
     <section class="index-treatments">
+        <div class="wrap">
+            <div class="grid">
+                <div>
+                    <div></div>
+                    <div>
+                        <h3>General Dentistry</h3>
+                        <p>Competently parallel task researched data process.</p>
+                    </div>
+                </div>
+                <div>
+                    <div></div>
+                    <div>
+                        <h3>Implants</h3>
+                        <p>Leverage other resource leveling convergence data.</p>
+                    </div>
+                </div>
+                <div>
+                    <div></div>
+                    <div>
+                        <h3>Dental Surgery</h3>
+                        <p>Engage fully tested process top line platform clients.</p>
+                    </div>
+                </div>
+                <div>
+                    <div></div>
+                    <div>
+                        <h3>Alignment</h3>
+                        <p>User generated content in real time offshoring.</p>
+                    </div>
+                </div>
+                <div>
+                    <div></div>
+                    <div>
+                        <h3>Teeth Braces</h3>
+                        <p>Synthesize integrated schemas with networks.</p>
+                    </div>
+                </div>
+                <div>
+                    <div></div>
+                    <div>
+                        <h3>Whitening</h3>
+                        <p>Incubate standards great channels scalable benefits.</p>
+                    </div>
+                </div>
+                <div>
+                    <div></div>
+                    <div>
+                        <h3>Teeth Protection</h3>
+                        <p>Evisculate holistic innovation rather than centric data.</p>
+                    </div>
+                </div>
+                <div>
+                    <div></div>
+                    <div>
+                        <h3>Prosthesis</h3>
+                        <p>Revolutionize global sources through dental services.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="free-q">
+        <div class="wrap">
+            <div>
+                <div>
+                    <div>
+                        <h2>Get A Free Quote</h2>
+                    </div>
+                    <div class="para">
+                        <p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
+                    </div>
+                    <div class="form-q">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="mb-3">
+                                        <input type="text" placeholder="Your name *" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="mb-3">
+                                        <input type="email" placeholder="Your Email Address *" class=" form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <textarea class="form-control" placeholder="Message and dental procedure you need *" id="" cols="30" rows="10"></textarea>
+                                </div>
+                                <div>
+                                    <button>Get A Qoute Now</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <img src="assets/images/img-oscar.jpg" alt="">
+            </div>
+        </div>
+    </section>
+    <section class="clients-with">
+        <div class="wrap">
+            <div class="heading-wrap">
+                <div>
+                    <h3>Clients With</h3>
+                    <h2>Reason To Smile</h2>
+                </div>
+                <div><button>All Client Testimonial</button></div>
+            </div>
+            <div class="team-carousel">
+                <div class="item">
+                    <div class="profile">
+                        <div class="headings">
+                            <h4>Jessica Bright</h4>
+                            <h5>Full Dental Implants</h5>
+                        </div>
+                        <div class="img-div">
+                            <img src="assets/images/jessica.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="para">
+                        <p>Dynamically target high-payoff intellectual capital for customized technologies and processes.</p>
+                    </div>
+                </div>
+                <div class="item"></div>
+                <div class="item"></div>
+                <div class="item"></div>
+                <div class="item"></div>
+                <div class="item"></div>
+            </div>
+        </div>
+    </section>
+    <section class="d-professionals">
+        <div class="wrap">
+            <div>
+                <img src="assets/images/img-dental-professionals.jpg" alt="">
+            </div>
+            <div>
+                <div>
+                    <div class="headings">
+                        <h3>The Dental</h3>
+                        <h2>Professionals</h2>
+                    </div>
+                    <div class="para">
+                        <p>DentiCare Studio is a well-established dentist in the heart of Budapest providing excellent dentistry for the whole family. Our priority is making you feel comfortable and at ease in a safe and welcoming environment.</p>
+                    </div>
+                    <div class="line"></div>
+                    <div class="pd-members">
+                        <h4>Proud Members</h4>
+                        <div>
+                            <img src="assets/images/logo-ada.png" alt="">
+                            <img src="assets/images/logo-adea.png" alt="">
+                            <img src="assets/images/logo-mouth-healthy.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="card-main">
+        <div class="wrap">
+            <div class="grid">
+                <div>
+                    <div class="bg-adam"></div>
+                    <div class="content">
+                        <h5>Clinic Director</h5>
+                        <h3>Adam <strong>Palmer</strong></h3>
 
+                        <div class="para">
+                            <p>The qualities of excellent communication and trust form part of our commitment to you and I have carefully chosen a great team of people.</p>
+                        </div>
+                        <div class="btn-logos">
+                            <button>Read More</button>
+                            <div>
+                                <a href=""><span><i class="fa-brands fa-facebook-f"></i></span></a>
+                                <a href=""><span><i class="fa-brands fa-twitter"></i></span></a>
+                                <a href=""><span><i class="fa-brands fa-linkedin-in"></i></span></a>
+                                <a href=""><span><i class="fa-brands fa-pinterest-p"></i></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="perfect-smile">
+        <div class="wrap">
+            <div>
+                <div class="headingss">
+                    <h3>Perfect Smile</h3>
+                    <h2>Excellent Defined</h2>
+                </div>
+                <div class="para">
+                    <p>Teeth whitening is one of the quickest ways to improve your smile. Many patients are amazed that one trip to DentiCare can change their teeth dramatically.</p>
+                </div>
+                <div class="progresses">
+                    <div class="entity">
+                        <p>Dental Satisfaction</p>
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                    <div class="entity">
+                        <p>Success Rate</p>
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                    <div class="entity">
+                        <p>Travel Satisfaction</p>
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="dental-travel">
+        <div class="wrap">
+            <div>
+                <div class="headings">
+                    <h3>Start Your</h3>
+                    <h2>Dental Travel</h2>
+                </div>
+                <div class="para">
+                    <p>It is well known that Hungary has become a main tourism destination. Besides its charm and attractiveness, Hungary has developed to a top level in dental care and became a remarkable dental tourism location for foreign patients.</p>
+                </div>
+            </div>
+        </div>
     </section>
     <?php
     include_once "footer.php"
